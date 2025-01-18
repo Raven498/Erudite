7a244d9c4bbc164332316aadcfc2057b44b01dba
Raven498 <78182016+Raven498@users.noreply.github.com>
2025-01-17 20:24:55 -0500

    develop journal transfer

--- Agent.java
+++ Agent.java
@@ -17,7 +17,7 @@ public class Agent {
     //Mainloop method
     public void cycle(){
         for(Environment env : trueKB.getEnvSet()){
-
+            
         }
     }
 
@@ -182,7 +182,7 @@ public class Agent {
         System.out.println(0.80 * (stdev(train_t) / stdev(f)));
 
 
-        //GOAL CREATION
+        //POLICY CREATION
 
 
         //STEP 2: PRL-POWERED EXPLORATION
