1796ed21d274532b6bf42e4df058d882f6720d9e
Nikilesh <78182016+Raven498@users.noreply.github.com>
2025-05-23 17:56:08 -0400

    prl demo algorithm approx setup (in progress)

--- Algorithm.java
+++ Algorithm.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 public class Algorithm extends Knowledge {
     public Concept input;
     public Concept output;
-    public String action;
+    public ArrayList<String> actions;
 
     public ArrayList<GenState> train_states;
 
-    public Algorithm(String name, String content, Agent.KClasses kclass, Concept input, Concept output, ArrayList<GenState> train_states) {
+    public Algorithm(String name, String content, Agent.KClasses kclass, Concept input, Concept output, ArrayList<String> actions) {
         super(name, content, kclass);
         this.input = input;
         this.output = output;
-        this.train_states = train_states;
+        this.actions = actions;
     }
 }
--- Main.java
+++ Main.java
@@ -9,9 +9,15 @@ public class Main {
         //TEST TKB ENV_SET
         ArrayList<Environment> test_env_set = new ArrayList<>();
         Environment env = new Environment();
-        Concept k = new Concept("POLYNOMIAL", "int a, int x, int n", Agent.KClasses.CONCEPT, new ArrayList<>(
+        Concept k = new Concept("MONOMIAL", "int a, int x, int n", Agent.KClasses.CONCEPT, new ArrayList<>(
                 Arrays.asList("a", "x", "n") //(NOTE: attr labels should be autopopulated by parsing content field)
         ));
+        ArrayList<String> acts = new ArrayList<>(Arrays.asList(
+                "O.a = MULT-I.a-I.n",
+                "O.x = I.x",
+                "O.n = ADD-I.a-NEG-1"
+        ));
+        Algorithm a = new Algorithm("POWER", "O.a = I.a * I.n, O.x = I.x, O.n = I.n - 1", Agent.KClasses.ALGORITHM, k, k, acts);
         env.addKnowledge(k);
         test_env_set.add(env);
         tkb.setEnvSet(test_env_set);
