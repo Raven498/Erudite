baea33d2dc835c0cf4cacd20492561a424454a87
Raven498 <78182016+Raven498@users.noreply.github.com>
2025-09-04 01:28:46 -0400

    graph reward vs input corrs, draft for reward corr detection

--- src/main/java/com/erudite/erudite_node/matrix/simple_agent.py
+++ src/main/java/com/erudite/erudite_node/matrix/simple_agent.py
@@ -1,4 +1,7 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
+
 class Monomial:
     a = 0
     x = ""
@@ -13,6 +16,13 @@ class Monomial:
         if isinstance(other, Monomial) and self.a == other.a and self.x == other.x and self.n == other.n:
             return True
         return False
+
+inputs = []
+truths = []
+outputs = []
+ry = 100
+rn = 50
+rewards = []
 
 def optimal_policy(mono):
     a = 0
@@ -36,15 +46,9 @@ def base_policy(mono):
     x = mono.x
     return Monomial(a, x, n)
 
-inputs = []
-truths = []
-outputs = []
-ry = 100
-rn = 50
-rewards = []
 def gen(n):
     for i in range(n):
-        mono = Monomial(random.randint(0, 100), "x", random.randint(0, 100))
+        mono = Monomial(i, "x", i) # Can also do random numbers instead
         inputs.append(mono)
         truth = optimal_policy(mono)
         output = base_policy(mono)
@@ -56,26 +60,34 @@ def gen(n):
             rewards.append(rn)
 
 def analyze():
-    regions = []
-    for i in range(len(rewards)):
-        
-            
-def corr_exp():
-    
-    '''
-input_monos = [
-    Monomial(5, x, 2),
-    Monomial(4, x, 1),
-    Monomial(9, x, 5),
-    Monomial(3, x, 10),
-    Monomial(20, x, 7),
-    Monomial(35, x, 7),
-    Monomial(27, x, 6),
-    Monomial(5, x, 1),
-    Monomial(5, x, 5),
-    Monomial(7, x, 10)]
+    input_a = []
+    input_n = []
+    input_x = []
+    # Step 1: Graph R vs input a, x, n
+    for i in range(len(rewards)): # Populate input a respective to rewards
+        input_a.append(inputs[i].a)
+        input_n.append(inputs[i].n)
+        input_x.append(inputs[i].x)
+    print(input_n)
+    print(rewards)
+    plt.plot(np.array(input_a), np.array(rewards))
+    plt.show()
+    j = 0
 
-truth_monos = [
-    Monomial(75, x, 15),
-    Monomial(
     '''
+    Algorithm Draft for Corr Detection (with R vs I.a, I.x, I.n):
+    For each input attr:
+        Iterate through rewards:
+            If current reward is rn and previous reward is ry (reward switches from ry -> rn):
+                Save input attr value corresponding to previous reward
+                (Target condition = x <= i, where i = previous input attr value, x = label for input attr)
+                OR (Target condition = x < y, where y = current input attr value, x = label for input attr)
+
+    Need to investigate what happens when I.n follows different target conditions in optimal policy than I.a --> what will reward graphs be?
+    '''
+    
+def corr_exp():
+    pass
+
+gen(100)
+analyze()
