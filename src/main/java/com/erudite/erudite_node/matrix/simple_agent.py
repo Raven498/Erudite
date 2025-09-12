84edad44e79f7ad66699fddcab438f085b96bdd0
Raven498 <78182016+Raven498@users.noreply.github.com>
2025-09-12 01:18:36 -0400

    simple agent demo: graphing output vs input attrs

--- src/main/java/com/erudite/erudite_node/matrix/simple_agent.py
+++ src/main/java/com/erudite/erudite_node/matrix/simple_agent.py
@@ -1,6 +1,8 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn
+from scipy.interpolate import CubicSpline
 
 class Monomial:
     a = 0
@@ -63,17 +65,67 @@ def analyze():
     input_a = []
     input_n = []
     input_x = []
+    truth_a = []
+    truth_x = []
+    truth_n = []
     # Step 1: Graph R vs input a, x, n
     for i in range(len(rewards)): # Populate input a respective to rewards
         input_a.append(inputs[i].a)
         input_n.append(inputs[i].n)
         input_x.append(inputs[i].x)
+        truth_a.append(truths[i].a)
+        truth_n.append(truths[i].n)
+        truth_x.append(truths[i].x)
+        
     print(input_n)
+    print(input_x)
     print(rewards)
+    
     plt.plot(np.array(input_a), np.array(rewards))
     plt.show()
-    j = 0
+    labels = ["a", "x", "n"]
+    in_label_reg = [input_a, input_x, input_n]
+    tru_label_reg = [truth_a, truth_x, truth_n]
+    regs = [in_label_reg, tru_label_reg]
+    targets = []
+    for i in range(len(labels)):
+        conds = []
+        for j in range(len(rewards)):
+            if j > 0 and (rewards[j] == rn and rewards[j-1] == ry):
+                conds.append(labels[i] + "<=" + str(in_label_reg[i][j]))
+        targets.append(conds)
+    print(targets)
 
+    for i in range(len(tru_label_reg)):
+        for j in range(len(in_label_reg)):
+            print("T." + labels[i] + " vs I." + labels[j])
+            plt.plot(np.array(in_label_reg[j]), np.array(tru_label_reg[i]))
+            plt.show()
+    
+    '''
+    cs = CubicSpline(np.array(input_a), np.array(truth_a))
+    d = cs.derivative()(input_a)
+    print("D" + str(d))
+    
+    '''
+    '''
+    poly = sklearn.preprocessing.PolynomialFeatures(degree=2)
+    X_poly = poly.fit_transform(np.array(input_a).reshape(-1, 1))
+
+    poly.fit(X_poly, truth_a)
+    lin2 = sklearn.linear_model.LinearRegression()
+    lin2.fit(X_poly, truth_a)
+
+    plt.scatter(input_a, truth_a, color='blue')
+
+    plt.plot(input_a, lin2.predict(poly.fit_transform(np.array(input_a).reshape(-1, 1))), color='red')
+    plt.title('Linear Regression')
+    plt.xlabel('Temperature')
+    plt.ylabel('Pressure')
+
+    plt.show()
+    '''
+    
     '''
     Algorithm Draft for Corr Detection (with R vs I.a, I.x, I.n):
     For each input attr:
@@ -85,6 +137,7 @@ def analyze():
 
     Need to investigate what happens when I.n follows different target conditions in optimal policy than I.a --> what will reward graphs be?
     '''
+
     
 def corr_exp():
     pass
