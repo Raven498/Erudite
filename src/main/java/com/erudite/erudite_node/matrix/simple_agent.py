67d34b215b89887cd7e70243453d96fc0f3d1424
Raven498 <78182016+Raven498@users.noreply.github.com>
2025-09-13 14:56:45 -0400

    simpleagent demo: bug fix attempts for calculating diffs

--- src/main/java/com/erudite/erudite_node/matrix/simple_agent.py
+++ src/main/java/com/erudite/erudite_node/matrix/simple_agent.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
 from scipy.interpolate import CubicSpline
-
+import heapq
 class Monomial:
     a = 0
     x = ""
@@ -99,8 +99,10 @@ def analyze():
     for i in range(len(tru_label_reg)):
         for j in range(len(in_label_reg)):
             print("T." + labels[i] + " vs I." + labels[j])
-            plt.plot(np.array(in_label_reg[j]), np.array(tru_label_reg[i]))
-            plt.show()
+            #plt.plot(np.array(in_label_reg[j]), np.array(tru_label_reg[i]))
+            #plt.show()
+
+    corr_exp(truth_a, input_a)
     
     '''
     cs = CubicSpline(np.array(input_a), np.array(truth_a))
@@ -137,8 +139,9 @@ def analyze():
 
     Need to investigate what happens when I.n follows different target conditions in optimal policy than I.a --> what will reward graphs be?
     '''
-
-    
+def sort(nums):
+   return heapq.heapify(nums.copy()) 
+ 
 def corr_exp(outputs, inputs):
     # Calculate diffs between all outputs to get final differentiation
     # TODO: Need to identify each unique function and its derivative - how to do this with final diff list?
@@ -147,14 +150,33 @@ def corr_exp(outputs, inputs):
             # Monitor each range for each function identified this way, and if each list only has 1 element, that's the final diff list
             # Use final diff list to get derivative, calculate original function
             # TODO: WRITE A SCRIPT TO TEST THIS
-    diffs = outputs.copy()
-    while len(set(diffs)) != 1:
-        for i in range(len(outputs)):
-            if i > 0:
-                diffs[i] = diffs[i] - diffs[i - 1]
     
-    # Calculate function
-
-
+    diffs = outputs.copy()
+    rel_change = []
+    for i in range(len(outputs)):
+        if i > 0 and outputs[i-1] != 0:
+            rel_change.append(((outputs[i] - outputs[i-1]) / outputs[i-1]) * 100)
+    j = 0
+    print("TRUTH A: " + str(outputs))
+    print("INPUT A: " + str(inputs))
+    while len(set(diffs)) >= 5:
+        for i in range(len(diffs)): 
+            if i > 0:
+                diffs[i] = diffs[i] - diffs[i-1]
+        if j < 3:
+            print(diffs)
+        j += 1
+    print("TRUTH A: " + str(outputs))
+    print("INPUT A: " + str(inputs))
+    print("DIFFS: " + str(diffs))
+    print("REL CHANGE: " + str(rel_change))
+    print("CUTOFF 1 REL CHANGE: " + str(rel_change[8]))
+    print("CUTOFF 2 REL CHANGE: " + str(rel_change[28]))
+    print(rel_change[8] == max(rel_change))
+    rel_change.append(0)
+    rel_change.append(0)
+    plt.plot(np.array(inputs), np.array(rel_change))
+    plt.show()
+    sort(rel_change)
 gen(100)
 analyze()
