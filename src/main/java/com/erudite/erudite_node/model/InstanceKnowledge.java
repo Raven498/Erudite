89f4530a17d4739c79746f93b2e39d891ed0e955
nikileshk41 <nrkashpro@gmail.com>
2026-03-13 03:25:17 -0400

    minor fix for instance class

--- src/main/java/com/erudite/erudite_node/model/InstanceKnowledge.java
+++ src/main/java/com/erudite/erudite_node/model/InstanceKnowledge.java
@@ -8,7 +8,7 @@ public class InstanceKnowledge extends Knowledge{
     public Object[] values;
 
     public InstanceKnowledge(String name, String content, Agent.KClasses kclass, Concept c){
-        //super(name, content, kclass);
+        super(name, content, kclass);
         this.c = c;
         this.values = new String[c.attr_labels.size()];
     }
