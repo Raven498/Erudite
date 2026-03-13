eff5a36c71537ebcb12285f519fb64c05c93ec66
nikileshk41 <nrkashpro@gmail.com>
2026-03-13 03:24:48 -0400

    pot, color knowledge initialization

--- src/main/java/com/erudite/erudite_node/model/InstanceKnowledge.java
+++ src/main/java/com/erudite/erudite_node/model/InstanceKnowledge.java
@@ -5,18 +5,18 @@ import com.erudite.erudite_node.service.Agent;
 //Turn into Generic to support more datatypes
 public class InstanceKnowledge extends Knowledge{
     public Concept c;
-    public String[] values;
+    public Object[] values;
 
     public InstanceKnowledge(String name, String content, Agent.KClasses kclass, Concept c){
-        super(name, content, kclass);
+        //super(name, content, kclass);
         this.c = c;
         this.values = new String[c.attr_labels.size()];
     }
 
-    public void addValue(String attr, String v){
+    public void addValue(String attr, Object v){
         values[c.attr_labels.indexOf(attr)] = v;
     }
-    public String getValue(String attr){
+    public Object getValue(String attr){
         return values[c.attr_labels.indexOf(attr)];
     }
 
--- src/main/java/com/erudite/erudite_node/ql_demo/Demo.java
+++ src/main/java/com/erudite/erudite_node/ql_demo/Demo.java
@@ -10,13 +10,33 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Demo {
-    Concept c1 = new Concept("Color", "C2", Agent.KClasses.CONCEPT,
-            new ArrayList<>(Arrays.asList("str name", "double hue", "double value", "double sat")));
-    Knowledge k1 = new Concept("Pot", "C1", Agent.KClasses.CONCEPT,
-            new ArrayList<>(Arrays.asList("C2 color")));
-    Concept c2 = new Concept("Color", "C2", Agent.KClasses.CONCEPT,
-            new ArrayList<>(Arrays.asList("str name", "double hue", "double value", "double sat")));
-    Knowledge k2 = c2;
-    Knowledge k3 = new InstanceKnowledge("Blue", "of C2", Agent.KClasses.INSTANCE, c2);
+    public static void main(String[] args){
+        Concept c1 = new Concept("Pot", "C1", Agent.KClasses.CONCEPT,
+                new ArrayList<>(Arrays.asList("C2 color")));
+        Knowledge k1 = c1;
+        Concept c2 = new Concept("Color", "C2", Agent.KClasses.CONCEPT,
+                new ArrayList<>(Arrays.asList("str name", "double hue", "double value", "double sat")));
+        Knowledge k2 = c2;
+        InstanceKnowledge i1 = new InstanceKnowledge("Blue", "I1", Agent.KClasses.INSTANCE, c2);
+        Knowledge k3 = i1;
+        InstanceKnowledge i2 = new InstanceKnowledge("Red", "I2", Agent.KClasses.INSTANCE, c2);
+        Knowledge k4 = i2;
+        InstanceKnowledge i3 = new InstanceKnowledge("P1", "I3", Agent.KClasses.INSTANCE, c1);
+        i3.addValue("C2 color", i1);
+        Knowledge k5 = i3;
+        InstanceKnowledge i4 = new InstanceKnowledge("P2", "I4", Agent.KClasses.INSTANCE, c2);
+        i4.addValue("C2 color", i1);
+        Knowledge k6 = i4;
+        InstanceKnowledge i5 = new InstanceKnowledge("P3", "I5", Agent.KClasses.INSTANCE, c2);
+        i5.addValue("C2 color", i1);
+        Knowledge k7 = i5;
+        Concept c3 = new Concept("Current Pot", "C3", Agent.KClasses.CONCEPT,
+                new ArrayList<>(Arrays.asList("C1 pot")));
+        Knowledge k8 = c3;
+        InstanceKnowledge i6 = new InstanceKnowledge("CP1", "I6", Agent.KClasses.INSTANCE, c3);
+        i6.addValue("C1 pot", i3);
+        Knowledge k9 = i6;
+        // GOAL DEF HERE
+    }
 
 }
